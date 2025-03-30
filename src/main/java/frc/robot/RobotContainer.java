d0fe3886cb9b86584ddcac04f0d4da3b08e370fb
Logan Bachman <geek4life812@gmail.com>
2025-03-30 18:04:47 -0400

    Added the controls from 250's comp robot, removed unnecessary controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.AlgaeConstants;
+import frc.robot.Constants.ElevatorConstants;
 import frc.robot.Constants.GoalConstants;
 import frc.robot.subsystems.*;
 
@@ -15,6 +17,7 @@ public class RobotContainer {
   private final Vision m_camera = new Vision();
   private final Drivetrain m_swerve = new Drivetrain(m_camera);
   private final Elevator m_elevator = new Elevator();
+  private final Climber m_climber = new Climber();
   private final Coral m_coral = new Coral();
   private final Algae m_algae = new Algae();
 
@@ -29,9 +32,27 @@ public class RobotContainer {
   }
 
   public void configureButtonBindings() {
-    // Configure your button bindings here
-    
+    // Climb Commands
+    m_driverController.leftTrigger(50).onTrue(m_climber.toggleClimber());
+
+    // Elevator Commands for the Manipulator
+    m_manipController.povUp().onTrue(m_elevator.setElevatorHeight(ElevatorConstants.ELEVATOR_L3_HEIGHT));
+    m_manipController.povRight().onTrue(m_elevator.setElevatorHeight(ElevatorConstants.ELEVATOR_L2_HEIGHT));
+    m_manipController.povDown().onTrue(m_elevator.setElevatorHeight(ElevatorConstants.ELEVATOR_STOW_HEIGHT));
+    m_manipController.povLeft().onTrue(m_elevator.setElevatorHeight(400));
+
     m_manipController.a().onTrue(m_swerve.goToPose(GoalConstants.BLUE_REEF_A,true));
+    
+    // Coral Flume Commands for the Manipulator
+    m_manipController.leftBumper().onTrue(m_coral.autoLockRelease());
+    
+    // Algae Holder Commands for the Manipulator
+    m_manipController.rightBumper().onTrue(m_algae.toggleAlgaeHolder());
+    
+    // Alage Tusk Commands for the Manipulator
+    m_manipController.y().onTrue(m_algae.setTuskPosition(AlgaeConstants.HOME_POSITION));
+    m_manipController.x().onTrue(m_algae.setTuskPosition(AlgaeConstants.APPROACH_POSITION));
+    m_manipController.a().onTrue(m_algae.setTuskPosition(AlgaeConstants.LOWER_POSITION));
   }
 
   public Command getAutonomousCommand() {
